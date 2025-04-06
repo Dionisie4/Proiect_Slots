3e23cd304bac42f06cce8153908362bb812949db
Dionisie4 <dionisiehagiu2@gmail.com>
2025-04-06 10:25:00 +0300

    Completata clasa Utilizator

---
+++ Pacanea.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proiect_Slots
+{
+    class Pacanea
+    {
+        private int mizaCurenta;
+        private static readonly List<int> MizePermise = new List<int> { 1, 2, 3, 5, 10, 20 };
+        
+        public void SeteazaMiza(int valoare)
+        {
+            bool ValoareCorecta = false;
+
+            if (MizePermise.Contains(valoare))
+            {
+                mizaCurenta = valoare;
+            }
+            else
+            {
+                throw new ArgumentException($"Valoarea mizei este incorecta, miza trebuie sa fie una din urmatoarele valori: {string.Join(", ", MizePermise)}");               
+            }
+
+
+
+        }
+
+    }
+
+}
--- Utilizator.cs
+++ Utilizator.cs
@@ -8,5 +8,52 @@ namespace Proiect_Slots
 {
     class Utilizator
     {
+        private string nume;
+        private string parola;
+        private int balanta;
+        private int zileDeLaUltimaLogare;
+        
+
+        public string Nume
+        {
+            get { return nume; }
+            set
+            {
+                if (value.Length <= 10)
+                {
+                    nume = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Numele trebuie să fie de maxim 10 caractere!");
+                }
+            }
+        }
+
+        public string Parola
+        {
+            get { return parola; }
+            protected set { parola = value; }
+        }
+
+        public int Balanta
+        {
+            get { return balanta; }
+            protected set { balanta = value; }
+        }
+        public int ZileDeLaUltimaLogare
+        {
+            get { return zileDeLaUltimaLogare; }
+            private set { zileDeLaUltimaLogare = value; }
+        }
+
+        public Utilizator (string Nume, string Parola, int Balanta, int ZileDeLaUltimaLogare)
+        {
+            nume = Nume;
+            parola = Parola;
+            balanta = Balanta;
+            zileDeLaUltimaLogare = ZileDeLaUltimaLogare;
+        }
     }
 }
+
